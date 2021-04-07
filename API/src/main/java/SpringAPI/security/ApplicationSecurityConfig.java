97350694c22481f6191b59e5575d6b5fcd119553
jacobbarnes <57917206+jacobbarnes@users.noreply.github.com>
2021-04-07 14:15:53 -0600

    JWT and UserDao

--- API/src/main/java/SpringAPI/security/ApplicationSecurityConfig.java
+++ API/src/main/java/SpringAPI/security/ApplicationSecurityConfig.java
@@ -1,57 +1,71 @@
 package SpringAPI.security;
 
-import static SpringAPI.security.ApplicationUserRole.ADMIN;
 import static SpringAPI.security.ApplicationUserRole.USER;
+import static SpringAPI.security.ApplicationUserRole.ADMIN;
+
+import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import SpringAPI.auth.ApplicationUserService;
+import SpringAPI.jwt.JwtConfig;
+import SpringAPI.jwt.JwtTokenVerifier;
+import SpringAPI.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private final PasswordEncoder passwordEncoder;
+	private final ApplicationUserService applicationUserService;
+	private final SecretKey secretKey;
+	private final JwtConfig jwtConfig;
 
 	@Autowired
-	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {this.passwordEncoder = passwordEncoder;}
+	public ApplicationSecurityConfig(
+					PasswordEncoder passwordEncoder,
+					ApplicationUserService applicationUserService,
+					SecretKey secretKey,
+					JwtConfig jwtConfig) {
+		this.passwordEncoder = passwordEncoder;
+		this.applicationUserService = applicationUserService;
+		this.secretKey = secretKey;
+		this.jwtConfig = jwtConfig;
+	}
 
 	@Override protected void configure(HttpSecurity http) throws Exception {
 		http
+						.csrf().disable()
+						.sessionManagement()
+							.sessionCreationPolicy(SessionCreationPolicy.ALWAYS.STATELESS)
+						.and()
+						.addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+						.addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
 						.authorizeRequests()
 						.antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-						.antMatchers("/api/**").hasRole(USER.name())
+						.antMatchers("/api/**").hasRole(ADMIN.name())
 						.anyRequest()
-						.authenticated()
-						.and()
-						.httpBasic();
+						.authenticated();
 	}
 
-	@Override
-	@Bean
-	protected UserDetailsService userDetailsService() {
-		UserDetails annaSmithUser = User.builder()
-						.username("annasmith")
-						.password(passwordEncoder.encode("password"))
-						.roles(USER.name()) // ROLE_USER
-						.build();
-		UserDetails lindaUser = User.builder()
-						.username("linda")
-						.password(passwordEncoder.encode("password123"))
-						.roles(ADMIN.name())
-						.build();
+	@Override protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(daoAuthenticationProvider());
+	}
 
-		return new InMemoryUserDetailsManager(
-						annaSmithUser,
-						lindaUser
-		);
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(passwordEncoder);
+		provider.setUserDetailsService(applicationUserService);
+		return provider;
 	}
 }
